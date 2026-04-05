36e2a9d00af5940a1e7fa3b6715a215273eea338
Serdot <sergio.siclari@outlook.com>
2026-04-05 19:18:15 +0200

    feat: add godotenv support and configure server address via environment variables

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.4
 
 require (
 	github.com/go-chi/chi/v5 v5.2.5
+	github.com/joho/godotenv v1.5.1
 	github.com/lrstanley/go-ytdlp v1.3.5
 	github.com/stretchr/testify v1.11.1
 )
--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/Sergio-dot/urtube/internal/router"
 	"github.com/Sergio-dot/urtube/internal/server"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+
+	serverHost := os.Getenv("SERVER_HOST")
+	serverPort := os.Getenv("SERVER_PORT")
+
 	router := router.NewRouter()
-	srv, err := server.NewServer("localhost:8080", router)
+	srv, err := server.NewServer(fmt.Sprintf("%s:%s", serverHost, serverPort), router)
 	if err != nil {
 		log.Fatalf("%v\n", err)
 	}
